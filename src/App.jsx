519969c8cc94d4e3fb0b64a7d5d6ec75bd436d4a
Yefry Sanchez <yefrymsp@gmail.com>
2024-08-01 16:05:15 -0400

    Sidebar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <main className="bg-darkBackground min-h-screen text-white font-inter px-4">
+    <main className="bg-darkBackground min-h-screen  text-white font-inter px-4 lg:p-6">
       <Navbar />
+      <SideBar />
       <AppRouter />
     </main>
   );
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const SideBar = () => {
+  const links = [
+    { title: "Home", icon: "fa-house", url: "/" },
+    { title: "Components", icon: "fa-code", url: "/components" },
+    { title: "Templates", icon: "fa-file-code", url: "/templates" },
+    { title: "About", icon: "fa-address-card", url: "/about" },
+    { title: "Contact", icon: "fa-message", url: "/contact" },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header
+      className={`hidden relative lg:flex flex-col   ${
+        isOpen
+          ? "w-52 px-3 transition-all duration-500 overflow-hidden"
+          : "w-20 items-center transition-all duration-500"
+      } py-8 rounded-2xl border border-softDarkText/30 bg-navyBG `}
+    >
+      {/* profile pic /////////////////*/}
+      <div className="mb-12 flex items-center gap-2">
+        <div className="h-10 w-10 rounded-full overflow-hidden">
+          <img
+            className="h-full w-full object-cover scale-[2]"
+            src="/profile.jpeg"
+            alt="profile picture"
+          />
+        </div>
+        {isOpen ? (
+          <div className="text-nowrap">
+            <h2 className="text-sm font-bold tracking-tighter mb-[2px]">
+              Yefry Sanchez
+            </h2>
+            <div className="flex items-center gap-2">
+              <div className="h-2 w-2 bg-accent rounded-full">
+                <div className="h-2 w-2 bg-accent rounded-full animate-ping"></div>
+              </div>
+
+              <p className="text-xs text-softDarkText"> Open to work</p>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      {/* profile pic/////////////////////////// */}
+
+      {/* btn open /////////////////////*/}
+      <button className="z-50" onClick={() => setIsOpen(!isOpen)}>
+        <i
+          className={`fa-solid fa-arrow-right absolute transition-all ${
+            isOpen
+              ? "right-4 top-9 rotate-180 transition-all duration-500"
+              : "-right-4 top-9 transition-all duration-500"
+          } text-softDarkText p-2 border border-softDarkText/30 rounded-full bg-darkBackground`}
+        ></i>
+      </button>
+
+      {/* btn open /////////////////////*/}
+
+      <ul className="flex flex-col gap-4">
+        {links.map((link, i) => (
+          <NavLink 
+            key={i}
+            to={link.url}
+            className={({ isActive }) => `${
+              isActive
+                ? "bg-darkBackground py-3 px-4 rounded-xl border border-softDarkText/30"
+                : "text-softDarkText py-3 px-4"
+            }
+                
+                
+            }`}
+          >
+            <li className="w-full flex items-center gap-2">
+              <i className={`fa-solid ${link.icon} ${isOpen ? "" : "mx-auto"}`}></i>
+              {isOpen ? <span>{link.title}</span> : ""}
+            </li>
+          </NavLink>
+        ))}
+      </ul>
+    </header>
+  );
+};
+
+export default SideBar;
